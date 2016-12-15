15055e3bd750d8253f01e19a57cede9348bfb89f
michael <msolorio789@gmail.com>
2016-12-14 20:00:31 -0800

    adds kombucha post request and automatic list update

--- public/main.js
+++ public/main.js
@@ -1,12 +1,20 @@
-const KombuchasForm = {
-
-	dom: {},
+const Kombuchas = {
 
 	loadElements() {
-		this.dom.kombuchaList = document.getElementById('js-kombuchaList');
-		this.dom.buttonAdd = document.getElementById('js-button-add');
-		this.dom.buttonUpdate = document.getElementById('js-button-update');
-		this.dom.buttonDelete = document.getElementById('js-button-delete');
+		this.kombuchaList = document.getElementById('js-kombuchaList');
+
+		// add form elements
+		this.buttonAdd = document.getElementById('js-button-add');
+		this.nameAdd = document.getElementById('js-name-add');
+		this.typeAdd = document.getElementById('js-type-add');
+		this.quantityAdd = document.getElementById('js-quantity-add');
+
+		// update form elements
+		this.buttonUpdate = document.getElementById('js-button-update');
+
+		// delete form elements
+		this.buttonDelete = document.getElementById('js-button-delete');
+
 	},
 
 	loadKombuchas() {
@@ -47,33 +55,90 @@ const KombuchasForm = {
 			kombuchaType.className = 'kombuchaProp kombuchaType';
 			kombuchaQuantity.className = 'kombuchaProp kombuchaQuantity';
 
-			//
+			// add id to entry
+			kombuchaEl.id = response[i]._id;
+
 			kombuchaEl.appendChild(kombuchaName);
 			kombuchaEl.appendChild(kombuchaType);
 			kombuchaEl.appendChild(kombuchaQuantity);
-
 			kombuchaFragment.appendChild(kombuchaEl);
 		}
-		this.dom.kombuchaList.appendChild(kombuchaFragment);
+		this.kombuchaList.innerHTML = "";
+		this.kombuchaList.appendChild(kombuchaFragment);
 	},
 
 	addKombucha() {
 		console.log('in addKombucha()');
+		nameAddVal = this.nameAdd.value;
+		typeAddVal = this.typeAdd.value;
+		quantityAddVal = this.quantityAdd.value;
+
+		const params = `name=${nameAddVal}&type=${typeAddVal}&quantity=${quantityAddVal}`;
+		console.log()
+		const xhr = new XMLHttpRequest();
+
+		const processRequest = () => {
+			console.log('in processRequest()');
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				// parse the string repsonse text into json
+				const response = JSON.parse(xhr.responseText);
+
+				// create elements
+				const kombuchaEl = document.createElement('div');
+				const kombuchaName = document.createElement('h3');
+				const kombuchaType = document.createElement('p');
+				const kombuchaQuantity = document.createElement('p');
+
+				// add text
+				kombuchaName.appendChild( document.createTextNode(response.data.name) );
+				kombuchaType.appendChild( document.createTextNode(`Type: ${response.data.type}`) );
+				kombuchaQuantity.appendChild( document.createTextNode(`Quantity: ${response.data.quantity}`) );
+
+				// add classes
+				kombuchaEl.className = 'kombuchaEntry';
+				kombuchaName.className = 'kombuchaProp kombuchaName';
+				kombuchaType.className = 'kombuchaProp kombuchaType';
+				kombuchaQuantity.className = 'kombuchaProp kombuchaQuantity';
+
+				// add id to entry
+				kombuchaEl.id = response.data._id;
+
+				kombuchaEl.appendChild(kombuchaName);
+				kombuchaEl.appendChild(kombuchaType);
+				kombuchaEl.appendChild(kombuchaQuantity);
+				this.kombuchaList.appendChild(kombuchaEl);
+			}
+		};
+
+		xhr.open('POST', '/api/kombuchas/', true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(params);
+		xhr.onreadystatechange = processRequest;
+
+		// this.loadKombuchas();
+
 	},
 
 	updateKombucha() {
 		console.log('in updateKombucha()');
 	},
 
-	deleteKombuchas() {
+	deleteKombucha() {
 		console.log('in deleteKombuchas()');
+	},
+
+	addListeners() {
+		this.buttonAdd.addEventListener('click', this.addKombucha.bind(this));
+		this.buttonUpdate.addEventListener('click', this.updateKombucha.bind(this));
+		this.buttonDelete.addEventListener('click', this.deleteKombucha.bind(this));
 	},
 
 	init() {
 		this.loadElements();
 		this.loadKombuchas();
+		this.addListeners();
 	}
 }
 
-window.KombuchasForm = KombuchasForm;
-KombuchasForm.init();
+window.Kombuchas = Kombuchas;
+Kombuchas.init();
